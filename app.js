9bedef9291b0829da0864006f37762d298fb4196
AnkiaDev <ankiadev@gmail.com>
2019-11-26 16:43:41 +0100

    add image path, user wishlist function and other wishlist function

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var fs = require("fs"); // fs
 var app = express();
 
 app.use("/css", express.static("./css"));
+app.use("/images", express.static("./images"));
 
 // ------ LES ROUTES --------
 // Page par défaut
@@ -115,16 +116,23 @@ app.get("/user/:username/menu.html", function(req, res) {
 app.get("/user/:username/wishlist.html", function(req, res) {
   res.setHeader("Content-Type", "text/html");
 
-    const username = req.params.username;
   // Code de Guillaume
   // Récupérer la liste des souhaits sous forme d'un objet JSON
   // dans le dossier data en fonction du nom d'utilisateur.
   // ex : data/guillaume.json
-
-  const listDesWish = "";
-
-  // Renvoie le template de sa wishlist
-  res.render("3.ejs", listeDesWish );
+  const username = req.params.username;
+  const userWishListPath = `data/${username}.json`;
+  fs.readFile(userWishListPath, "utf8", function(err, data) {
+    if (err) {
+      throw err;
+    } else {
+      const jsonListOfWish = JSON.parse(data);
+      console.log("json dans le render", jsonListOfWish);
+      console.log("type du list of wish", typeof jsonListOfWish);
+      // Renvoie le template de sa wishlist
+      res.render("3.ejs", { list: jsonListOfWish });
+    }
+  });
 });
 
 // Ajout d'un souhait
@@ -136,7 +144,6 @@ app.get("/user/:username/add.html", function(req, res) {
   // Renvoie le template de l'ajout d'un élément de sa wishlist
   res.render("4.ejs");
 });
-
 
 // Suppression d'un souhait
 app.get("/user/:username/delete.html", function(req, res) {
@@ -190,9 +197,20 @@ app.get("/user/:username/others/:othername.html", function(req, res) {
   res.setHeader("Content-Type", "text/html");
 
   // Code de Guillaume
-
-  // Renvoie le template de la liste d'une autre personne humaine
-  res.render("8.ejs");
+  const othername = req.params.othername;
+  const userWishListPath = `data/${othername}.json`;
+  fs.readFile(userWishListPath, "utf8", function(err, data) {
+    if (err) {
+      throw err;
+    } else {
+      const jsonListOfWish = JSON.parse(data);
+      console.log("json dans le render", jsonListOfWish);
+      console.log("type du list of wish", typeof jsonListOfWish);
+      // Renvoie le template de la liste d'une autre personne humaine
+      res.render("8.ejs", { list: jsonListOfWish, user: othername });
+    }
+    //user.html?nom=nomduwish
+  });
 });
 
 // Détails d'un souhait
